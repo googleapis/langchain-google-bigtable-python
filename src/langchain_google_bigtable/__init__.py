c0f4244aacb997434d8a5dc1decdeb3da32f9140
ron-gal <125445217+ron-gal@users.noreply.github.com>
2024-02-07 11:48:13 -0500

    feat: BigtableLoader and BigtableSaver implementation (#5)
    * fix repo
    
    * feat: BigtableLoader and BigtableSaver implementation
    
    * add test
    
    * add some tests
    
    * add load test
    
    * fix load test
    
    * fix merge
    
    * Add tests
    
    * update doc pip install command
    
    * move tests to tests folder
    
    * Update docs
    
    * fix docs
    
    * memoize client
    
    * fixed PR comments

--- src/langchain_google_bigtable/__init__.py
+++ src/langchain_google_bigtable/__init__.py
@@ -11,3 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from langchain_google_bigtable.document_loader import (
+    BigtableLoader,
+    BigtableSaver,
+    Encoding,
+    MetadataMapping,
+)
+
+__all__ = ["BigtableLoader", "BigtableSaver", "MetadataMapping", "Encoding"]
